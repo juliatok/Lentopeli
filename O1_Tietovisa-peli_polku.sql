b2faf6f3e0583079e0609c674501bae33316f129
Suzu10 <122264254+Suzu10@users.noreply.github.com>
2023-02-13 12:15:15 +0200

    Add files via upload

--- O1_Tietovisa-peli_polku.sql
+++ O1_Tietovisa-peli_polku.sql
@@ -13,9 +13,33 @@ create table maat(
 insert into maat (ID, Name)
     VALUES (1, "Iso-Britannia"),
 	 (2, "Ranska"),
-	 (3, "Saksa"),
-	 (4, "Italia"),
-	 (5, "Espanja"),
-	 (6, "Kreikka");
+	 (3, "Italia"),
+	 (4, "Espanja"),
+	 (5, "Saksa"),
+	 (6, "Kreikka"),
+	 (7, "Suomi"),
+	 (8, "Ruotsi"),
+	 (9, "Romania"),
+	 (10, "Puola"),
+	 (11, "Bulgaria"),
+	 (12, "Slovenia"),
+	 (13, "Slovakia"),
+	 (14, "Itävalta"),
+	 (15, "Norja"),
+	 (16, "Viro"),
+	 (17, "Latvia"),
+	 (18, "Islanti"),
+	 (19, "Irlanti"),
+	 (20, "Portugali"),
+	 (21, "Alankomaat"),
+	 (22, "Ukraina"),
+	 (23, "Montenegro"),
+	 (24, "Kypros"),
+	 (25, "Malta"),
+	 (26, "Sveitsi"),
+	 (27, "Tsekki"),
+	 (28, "Belgia"),
+	 (29, "Serbia"),
+	 (30, "Moldova");
 	 
-
+	 
