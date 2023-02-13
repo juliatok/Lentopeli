cbca1276fa9961004ca59696fd41742619cedd29
Wimmu <wimmu.isberg@gmail.com>
2023-02-13 10:34:03 +0200

    Wimmulla toimii

--- Lentopeli.py
+++ Lentopeli.py
@@ -1,23 +1,46 @@
 syöte = 1
+pisteet = 300
 
+# ALOITUSRUUTU
 def mainmenu():
     print("Paina enter aloittaaksesi pelin.")
     print("Paina 0 lopettaaksesi ohjelman")
     syöte = input("->")
     return syöte
+
+# KÄYTTÄJÄNIMEN SYÖTTÖ
 def username():
     print("Anna käyttäjänimesi:")
     käyttäjänimi = input("->")
     return käyttäjänimi
+
+# KOTIMAAN VALINTA
 def maanvalinta():
     print("Hei", käyttäjänimi, "Valitse kotimaasi:")
     print("A. Suomi B. Ruotsi C. Tanska")
     valinta = input("->")
     return valinta
 
-mainmenu()
+# LOPPURUUTU
+def end():
+    print("Onneksi olkoon", käyttäjänimi, "läpäisit pelin!")
+    print("Sait", pisteet, "pistettä!")
+    print("Paina 1 palataksesi päävalikkoon")
+    print("Paina 0 lopettaaksesi ohjelman")
+    valinta = input("->")
+    while valinta != "1" and valinta != "0":
+        print("Virheellinen syöte!")
+        print("Paina 1 palataksesi päävalikkoon")
+        print("Paina 0 lopettaaksesi ohjelman")
+        valinta = input("->")
+    return valinta
+
+# PÄÄOHJELMA
 while syöte != "0":
-    if syöte != "0":
-        käyttäjänimi = username()
-        maanvalinta()
+    mainmenu()
+    käyttäjänimi = username()
+    maanvalinta()
+    valinta = end()
+    if valinta == "0":
+        break
 
