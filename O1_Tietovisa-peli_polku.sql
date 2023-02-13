6712106b1769602d8c2a5530ed68a4b9ad65a9ef
Suzu10 <122264254+Suzu10@users.noreply.github.com>
2023-02-13 11:13:43 +0200

    Add files via upload

--- O1_Tietovisa-peli_polku.sql
+++ O1_Tietovisa-peli_polku.sql
@@ -1,1 +1,21 @@
+DROP DATABASE if EXISTS tietovisa;
 create database tietovisa;
+
+use tietovisa;
+
+create table maat(
+    ID      INT       	 	 not null,
+    Name    VARCHAR(64)     not null,
+    PRIMARY KEY (ID)
+);
+
+
+insert into maat (ID, Name)
+    VALUES (1, "Iso-Britannia"),
+	 (2, "Ranska"),
+	 (3, "Saksa"),
+	 (4, "Italia"),
+	 (5, "Espanja"),
+	 (6, "Kreikka");
+	 
+
