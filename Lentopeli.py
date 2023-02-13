86c1af5fb74fc189c682386b13c72d82213fceb3
juliatok <Julia.Tokola@metropolia.fi>
2023-02-13 10:54:00 +0200

    Tee virheilmoitukset

--- Lentopeli.py
+++ Lentopeli.py
@@ -5,20 +5,30 @@ pisteet = 300
 def mainmenu():
     print("Paina enter aloittaaksesi pelin.")
     print("Paina 0 lopettaaksesi ohjelman")
-    syöte = input("->")
+    syöte = input("-> ")
+    while syöte != "" and syöte != 0:
+        print("Virheellinen syöte!")
+        print("Paina enter aloittaaksesi pelin.")
+        print("Paina 0 lopettaaksesi ohjelman")
+        syöte = input("-> ")
     return syöte
 
 # KÄYTTÄJÄNIMEN SYÖTTÖ
 def username():
     print("Anna käyttäjänimesi:")
-    käyttäjänimi = input("->")
+    käyttäjänimi = input("-> ")
     return käyttäjänimi
 
 # KOTIMAAN VALINTA
 def maanvalinta():
     print("Hei", käyttäjänimi, "Valitse kotimaasi:")
     print("A. Suomi B. Ruotsi C. Tanska")
-    valinta = input("->")
+    valinta = input("-> ")
+    while valinta != "A" and valinta != "B" and valinta != "C":
+        print("Virheellinen syöte!")
+        print("Valitse kotimaasi:")
+        print("A. Suomi B. Ruotsi C. Tanska")
+        valinta = input("-> ")
     return valinta
 
 # LOPPURUUTU
@@ -27,12 +37,12 @@ def end():
     print("Sait", pisteet, "pistettä!")
     print("Paina 1 palataksesi päävalikkoon")
     print("Paina 0 lopettaaksesi ohjelman")
-    valinta = input("->")
+    valinta = input("-> ")
     while valinta != "1" and valinta != "0":
         print("Virheellinen syöte!")
         print("Paina 1 palataksesi päävalikkoon")
         print("Paina 0 lopettaaksesi ohjelman")
-        valinta = input("->")
+        valinta = input("-> ")
     return valinta
 
 # PÄÄOHJELMA
