768f691052e5d44fbd2557157e2f9e43821ce176
Suzu10 <122264254+Suzu10@users.noreply.github.com>
2023-02-28 14:16:47 +0200

    Add files via upload

--- O1_Tietovisa-peli_polku.sql
+++ O1_Tietovisa-peli_polku.sql
@@ -1,18 +1,12 @@
 # DROP DATABASE if EXISTS tietovisa;
 USE flight_game;
 
-DROP TABLE if EXISTS vastaukset;
-DROP TABLE if EXISTS pisteet;
-DROP TABLE if EXISTS maat;
-DROP TABLE if EXISTS käyttäjä;
-
-
 CREATE TABLE maat(
     ID      			INT       	 	NOT NULL,
     Nimi    			VARCHAR(64)    NOT NULL,
-    vastaukset_id 	INT				,
-	 Maakoodi			VARCHAR (2)		,	
-    PRIMARY KEY (ID)
+	 iso_country		VARCHAR (64)	,	
+    PRIMARY KEY (ID),
+    FOREIGN KEY (iso_country) REFERENCES country (iso_country)
 );
 
 CREATE TABLE vastaukset(
@@ -29,88 +23,82 @@ CREATE TABLE vastaukset(
 CREATE TABLE käyttäjä(
 	ID				INT 				NOT NULL auto_increment,
 	Nimi			VARCHAR(60)		NOT NULL,
-	paikka		INT 				,				
-	PRIMARY KEY (ID)
+	paikka_id	VARCHAR (60) 	,
+	pisteet 		INT 				,				
+	PRIMARY KEY (ID)				,
+	FOREIGN KEY	(paikka_id) REFERENCES airport (ident)
 );
 
-CREATE TABLE pisteet( 
-	ID				INT				NOT NULL AUTO_INCREMENT,
-	pisteet_id	INT				,
-	PRIMARY KEY (ID)				,
-	FOREIGN KEY (pisteet_id) REFERENCES käyttäjä (ID)
-);	
-
 INSERT INTO maat (ID, Nimi, Maakoodi)
-    VALUES (1, "Iso-Britannia","EN"),
-	 (2, "Ranska","FR"),
-	 (3, "Italia","IT"),
-	 (4, "Espanja","ES"),
-	 (5, "Saksa","DE"),
-	 (6, "Kreikka","GR"),
-	 (7, "Suomi","FI"),
-	 (8, "Ruotsi","SE"),
-	 (9, "Romania","RO"),
-	 (10, "Puola","PL"),
-	 (11, "Bulgaria","BG"),
-	 (12, "Slovenia","SI"),
-	 (13, "Slovakia","SK"),
-	 (14, "Itävalta","AT"),
-	 (15, "Norja","NO"),
-	 (16, "Viro","ES"),
-	 (17, "Latvia","LV"),
-	 (18, "Islanti","IS"),
-	 (19, "Irlanti","IE"),
-	 (20, "Portugali","PT"),
-	 (21, "Alankomaat","NL"),
-	 (22, "Ukraina","UA"),
-	 (23, "Montenegro","ME"),
-	 (24, "Kypros","CY"),
-	 (25, "Malta","MT"),
-	 (26, "Sveitsi","CH"),
-	 (27, "Tsekki","CZ"),
-	 (28, "Belgia","BE"),
-	 (29, "Serbia","RS"),
-	 (30, "Moldova","MD");
+    VALUES (1, "Iso-Britannia"),
+	 (2, "Ranska"),
+	 (3, "Italia"),
+	 (4, "Espanja"),
+	 (5, "Saksa"),
+	 (6, "Kreikka"),
+	 (7, "Suomi"),
+	 (8, "Ruotsi"),
+	 (9, "Romania"),
+	 (10, "Puola"),
+	 (11, "Bulgaria"),
+	 (12, "Slovenia"),
+	 (13, "Slovakia"),
+	 (14, "Itävalta"),
+	 (15, "Norja"),
+	 (16, "Viro"),
+	 (17, "Latvia"),
+	 (18, "Islanti"),
+	 (19, "Irlanti"),
+	 (20, "Portugali"),
+	 (21, "Alankomaat"),
+	 (22, "Ukraina"),
+	 (23, "Montenegro"),
+	 (24, "Kypros"),
+	 (25, "Malta"),
+	 (26, "Sveitsi"),
+	 (27, "Tsekki"),
+	 (28, "Belgia"),
+	 (29, "Serbia"),
+	 (30, "Moldova");
 	 
 	 
-INSERT INTO käyttäjä (Nimi)
-	VALUES ("Petra"),
-	("Mia"),
-	("Wilma"),
-	("Julia");
+INSERT INTO käyttäjä (Nimi, pisteet)
+	VALUES ("Petra", 0),
+	("Mia", 0),
+	("Wilma", 0),
+	("Julia", 0);
 	
 INSERT INTO vastaukset (paikka_id, kysymys, oikein, väärin1, väärin2)
 	VALUES (1,"Mikä on Britannian kansalliseläin?","Leijona","Kotka","Karhu"),
 	(1,"Mikä yhtye ei ole kotoisin Britanniasta?","Guns N' Roses","Queen","The Rolling Stones"),
 	(2,"Mikä on Ranskan korkein vuori?","Mont Blanc","Mount Everest","Monte Bianco"),
-	(2," "," "," "," "),
-	(3," "," "," "," "),
-	(3," "," "," "," "),
-	(4," "," "," "," "),
-	(4," "," "," "," "),
-	(5," "," "," "," "),
-	(5," "," "," "," "),
-	(6," "," "," "," "),
-	(6," "," "," "," "),
-	(7," "," "," "," "),
-	(7," "," "," "," "),
+	(2,"Kuinka korkea on Eiffel-torni?","324 metriä","264 metriä","412 metriä"),
+	(3,"Missä kaupungissa siaitsee kuuluisa kalteva torni?","Pisa","Napoli","Torino"),
+	(3,"Mikä on Italian suurin saari?","Sisilia","Sardinia","Elba"),
+	(4,"Missä kaupungissa on suurin väkiluku?","Madrid","Barcelona","Valencia"),
+	(4,"Mikä on Espanjan suurin saari?","Mallorca","Teneriffa","Gran Canaria"),
+	(5,"Mikä on Saksan suurin kaupunki väkiluvultaan?","Berliini","Hampuri","München"),
+	(5,"Mikä on Saksan suurin järvi?","Bodensee","Eibsee","Chiemsee"),
+	(6,"Mikä on Kreikan kansallisurheilulaji?","Paini","Jalkapallo","Viesti"),
+	(6,"Minkä niminen on Kreikan tunnetuin antiikin aikainen temppeli?","Parthenon"," Pantheon","Patheon"),
+	(7,"Mikä on Suomen pohjoisin kunta?","Utsjoki","Inari","Enontekijö"),
+	(7,"Kuka on Suomen seitsemäs presidentti?","Paasikivi","Ryti","Kekkonen"),
 	(8,"Mikä on Ruotsin toiseksi suurin kaupunki?","Göteborg","Tukholma","Malmö"),
-	(8," "," "," "," "),
-	(9," "," "," "," "),
-	(9," "," "," "," "),
-	(10," "," "," "," "),
-	(10," "," "," "," "),
-	(11," "," "," "," "),
-	(11," "," "," "," "),
-	(12," "," "," "," "),
-	(12," "," "," "," "),
-	(13," "," "," "," "),
-	(13," "," "," "," "),
-	(14," "," "," "," "),
-	(14," "," "," "," "),	
-	(15," "," "," "," "),	
-	(15," "," "," "," "),
-	(16,"Virossa sijaitseva ylioistokaupunki on nimeltään:","Tartto","Pärnu","Narva"),
+	(8,"Monta virallista vähemmistökieltä Ruotsilla on?","5 Kieltä","2 Kieltä","3 Kieltä"),
+	(9,"Monta naapurimaata Romanialla on?","5","6","7"),
+	(9,"Kehen Romanian hallitsijaan Dracula perustuu?","Vlad Tepes","István Báthory","János Hunyadi"),
+	(10,"Mikä on Puolan suurin uskonto?","Katolinen kirkko","Ortodoksinen kirkko","Protestantinen kirkko"),
+	(10,"Mikä on Puolan pisin joki?","Veiksel","Oder","Warta"),
+	(11,"Mikä on Bulgarian pääkaupunki?","Sofia","Vilna","Wien"),
+	(11,"Mikä on Bulgarian suurin satamakaupunki?","Varna","Burgas","Plovdiv"),
+	(12,"Mikä maa tunnusti Slovenian itesenäisyyden ensin?","Kroatia","Itävalta","Serbia"),
+	(12,"Mikä leivonnainen on kotoisin Slovakiasta?","Potica","Medimurska","Banket"),
+	(13,"Mitkä on Slovakian lipun värit?","Valkoinen, sininen ja punainen","Keltainen, vihreä ja punainen","Oranssi, valkoinen ja vihreä"),
+	(13,"Mikä on Slovakian katsotuin urheilulaji?","Jääkiekko","Jalkapallo","Tennis"),
+	(14,"Mikä on Itävallan virallinen kieli?","Saksa","Kroatia","Unkari"),
+	(14,"Monta osavaltiota Itävallalla on?","9","13","17"),
+	(15,"Keskeisimpiä elementtejä Norjan maisemassa ovat:","Vuonot ja vuoret","Joet ja järvet","Metsät ja tunturit"),
+	(15,"Norjan perustaja ja ensimmäinen kuningas oli:","Harald Kaunotukka","Pyhä Olavi","Eerik Verikirves"),(16,"Virossa sijaitseva ylioistokaupunki on nimeltään:","Tartto","Pärnu","Narva"),
 	(16,"Viron itsenäisyyspäivää juhlitaan:","24.helmikuuta","17.lokakuuta","4.kesäkuuta"),
 	(17,"Latvian väkiluku on:","Noin 2 miljoonaa","Noin 3,5 miljoonaa","Noin 6 miljoonaa"),
 	(17,"Latvia voitti Euroviisut vuonna:","2002","2011","2008"),
@@ -140,3 +128,4 @@ INSERT INTO vastaukset (paikka_id, kysymys, oikein, väärin1, väärin2)
 	(29,"Kuka on Serbian nykyinen presidentti?","Alexsandar Vucic","Novak Dokovic","Ana Brnabic"),
 	(30,"Kuinka korkea on Moldovan korkein kohta?","420 metriä","640 metriä","310 metriä"),
 	(30,"Mikä on Moldovan virallinen kieli?","Romanian kieli","Moldovan kieli","Unkarin kieli"); 
+	
