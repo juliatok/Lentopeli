9c06fc03d62f022b98ec6fa5e62a1c86d0726769
Suzu10 <petra.haukkala@gmail.com>
2023-03-06 16:49:47 +0200

    Käyttäjän lisäys tietokantaan

--- Petran_Testit.py
+++ Petran_Testit.py
@@ -17,26 +17,30 @@ def flight_game(icao):
     tulos = kursori.fetchall()
     return tulos"""
 
-def paikka(icao):
-    sql = "Select  from airport where ident = '" + icao + "'"
-    #print(sql)
-    kursori = yhteys.cursor()
-    kursori.execute(sql)
-    tulos = kursori.fetchall()
-    return tulos
 def username():
     print("Anna käyttäjänimesi:")
     käyttäjänimi = input("-> ")
-    sql = "insert into käyttäjä(nimi,pisteet) values ('" + käyttäjänimi + "', 0)"
-    print(sql)
-    kursori = yhteys.cursor()
-    kursori.execute(sql)
-    yhteys.commit()
     print("Tervetuloa",käyttäjänimi)
     return käyttäjänimi
 
 #def etäpisteet():
 
+
+# -- Tietokantaan lisäys --
+def top5(käyttäjänimi,pisteet):
+    """top5 = []
+    haku = "select pisteet from käyttäjä"
+    for rivi in haku:
+        points = rivi[0]
+        top5.append(points)
+    if pisteet > top5[-1]:"""
+    sql = "insert into käyttäjä(nimi,pisteet) values ('" + käyttäjänimi + "', '" + pisteet + "')"
+    print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    yhteys.commit()
+    print("Tervetuloa", käyttäjänimi)
+    return
 
 
 yhteys = mysql.connector.connect(
@@ -48,8 +52,9 @@ yhteys = mysql.connector.connect(
          autocommit=True
          )
 
-
 käyttäjänimi = username()           # KÄYTTÄJÄNIMEN KYSYNTÄ JA SEN LISÄYS TIETOKANTAAN
+pisteet = input("Anna pisteet:")
+top5lista = top5(käyttäjänimi,pisteet)
 
 '''icao = "EFHK"
 paikka1 = paikka(icao)
