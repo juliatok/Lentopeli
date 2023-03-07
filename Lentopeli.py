272dbf4ef55828e34623cf38e6814b516657a046
juliatok <Julia.Tokola@metropolia.fi>
2023-03-07 21:01:41 +0200

    Lisää laske lentomatkan pituus + korjaa kirjoitusvirheitä

--- Lentopeli.py
+++ Lentopeli.py
@@ -3,6 +3,8 @@ import geopy.distance
 import random
 
 # ______________________ ALOITUSRUUTU ______________________
+
+
 def mainmenu():
     print("____________________________________________________________________")
     print("  ")
@@ -29,6 +31,8 @@ def mainmenu():
     return syöte
 
 # ______________________ KÄYTTÄJÄNIMI ______________________
+
+
 def käyttäjänimivalinta():
     print("   • Anna käyttäjänimesi •")
     print(" ")
@@ -37,6 +41,8 @@ def käyttäjänimivalinta():
     return käyttäjänimi
 
 # ____________________ ARPOO LENTOKENTÄN MAALLE ____________________
+
+
 def etsimaanlentokenttä(maa):
     sql = "select airport.name from airport, maat where nimi = '" + str(maa) + "' and airport.iso_country = maat.iso_country and type = 'large_airport' order by rand() limit 1"
     # print(sql)
@@ -56,6 +62,8 @@ def etsimaanlentokenttä(maa):
         return tulos
 
 # ______________________ ARPOO 3 MAATA ______________________
+
+
 def arvokolmemaata():
     kerrat = 0
     arvotutmaat = []
@@ -75,11 +83,13 @@ def arvokolmemaata():
     return arvotutmaat
 
 # _________________ VALITSEE SEURAAVAN MAAN ______________________
+
+
 def arvolentokenttä(nykyinenmaa): # ARPOO KOLME MAATA JA LENTOKENTTÄÄ MIHIN LENTÄÄ SEURAAVAKSI
     arvotutmaat = arvokolmemaata()
     kerrat = 1
     print("  ")
-    print("   • Olet maassa", ''.join(nykyinenmaa[0]), "Valitse minne haluat lentää! •")
+    print("   • Olet maassa", ''.join(nykyinenmaa[0]), "- Valitse minne haluaisit lentää! •")
     print("  ")
     for n in arvotutmaat:
         if kerrat == 1:
@@ -116,12 +126,14 @@ def arvolentokenttä(nykyinenmaa): # ARPOO KOLME MAATA JA LENTOKENTTÄÄ MIHIN L
     return valinta
 
 # ______________________ KOTIMAAN VALINTA ______________________
+
+
 def kotimaanvalinta(käyttäjänimi):
     print("  ")
-    print("Tervetuloa pelaamaan Eurooppa Tietovisa peliä", käyttäjänimi, "!")
+    print("Tervetuloa pelaamaan Eurooppa Tietovisa -peliä", käyttäjänimi, "!")
     print(" ")
     print("Tässä pelissä kierrät läpi kymmenen Euroopan maata")
-    print("vastaten ei maihin liittyviin kysymyksiin.")
+    print("vastaten kussakin maassa maahan liittyvään tietovisa-kysymykseen.")
     print(" ")
     print("Aloita seikkailusi valitsemalla kotimaasi, josta aloitat pelin.")
     print("____________________________________________________________________")
@@ -159,7 +171,27 @@ def kotimaanvalinta(käyttäjänimi):
     print("__________________________________________________________________")
     return valinta
 
+# HAE RANDOM LENTOKENTTÄ KOTIMAASTA (matkan pituuden laskuun - ei näy pelissä):
+
+
+def hae_kotikentta(valittu_kotimaa):
+
+    count = 0
+
+    while count <= 1:
+
+        sql = "select airport.name from airport, maat " \
+              "where nimi = ""'" + str(valittu_kotimaa) + "' " \
+              "and airport.iso_country = maat.iso_country order by rand() limit 1"
+        cursor = yhteys.cursor()
+        cursor.execute(sql)
+        arvottu_kotikentta = cursor.fetchall()
+
+        return arvottu_kotikentta[0]
+
 # HAE VALITUN MAAN ID:
+
+
 def hae_id(sijainti):
 
     sql = "select ID from maat where Nimi = '" + sijainti[0] + "'"
@@ -170,6 +202,8 @@ def hae_id(sijainti):
     return result[0][0]
 
 # VALITUN MAAN KYSYMYS PELAAJALLE:
+
+
 def kysymys_pelaajalle(id, lennot):
 
     sql = "select ID, kysymys from vastaukset where paikka_id = '" + str(id) + "'"
@@ -188,12 +222,13 @@ def kysymys_pelaajalle(id, lennot):
 
 # VASTAUSVAIHTOEHDOT PELAAJALLE:
 
+
 def vastaus_vaihtoehdot(id):
 
     sql = "select oikein, väärin1, väärin2 from vastaukset where ID = '" + str(id) + "'"
     cursor = yhteys.cursor()
     cursor.execute(sql)
-    ret = list(cursor.fetchall()[0])
+    ret = list(cursor.fetchall()[0])    # tekee listan tuplesta
 
     vastausvaihtoehdot = []
     vastausvaihtoehdot.append([ret[0], "oikein"])
@@ -212,8 +247,8 @@ def vastaus_vaihtoehdot(id):
 
     return vastausvaihtoehdot
 
-# PELAAJAN VASTAUS:
-# palauttaa 0 jos vastaus oikein, -1 jos väärin
+# PELAAJAN VASTAUS (palauttaa 0 jos vastaus oikein, -1 jos väärin):
+
 
 def anna_vastaus(vastaukset):
 
@@ -226,34 +261,73 @@ def anna_vastaus(vastaukset):
 
     print("____________________________________________________________________")
     print("")
+
     for vastaus in vastaukset:
         if pelaajan_syote in vastaus:
             if vastaus[1] == "oikein":
-                print("   Oikein!")
                 return 0
             else:
-                print("   Väärin meni!")
                 return -1
+
     print("____________________________________________________________________")
 
 # ______________________ PISTEFUNKTIO ______________________
+
+
 def pisteidenlasku(pelaajan_vastaus, pisteet):
     if pelaajan_vastaus == 0:
         print("Oikein! Sait 80 pistettä!")
         pisteet = pisteet + 80
     else:
-        print("Väärin meni. Menetit 20 pistettä.")
+        print("Väärin meni! Menetit 20 pistettä.")
         pisteet = pisteet - 20
     print(f"Sinulla on nyt yhteensä {pisteet} pistettä.")
     return pisteet
 
+# HAKEE EDELLISEN JA NYKYISEN LENTOKENTÄN:
+
+
+def edellinen_nykyinen_lentokentta(kentat_lista):
+
+    edellinen = kentat_lista[-2][0]
+    nykyinen = kentat_lista[-1][0]
+
+    return edellinen, nykyinen
+
+# HAKEE LENTOKENTÄN KOORDINAATIT:
+
+
+def hae_koordinaatit(airport_name):
+
+    sql = "select latitude_deg, longitude_deg from airport where name = '" + airport_name + "'"
+    cursor = yhteys.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+
+    return result
+
+# LASKEE EDELLISEN JA NYKYISEN LENTOKENTÄN VÄLIMATKAN:
+
+
+def laske_valimatka(koordinaatit1, koordinaatit2):
+
+    print(f"Koordinaatit: {koordinaatit1}, {koordinaatit2}")
+    valimatka = geopy.distance.geodesic(koordinaatit1, koordinaatit2).km
+    print(f"Lensit välimatkalla {valimatka:.0f} km")
+
+    return valimatka
+
 # ______________________ LOPPURUUTU ______________________
+
+
 def end():
     print("  ")
     print("   Onneksi olkoon", käyttäjänimi, "läpäisit pelin!")
     print("   Olet taas kotimaassasi", ', '.join(kotimaa))
     print("   Sait", pisteet, "pistettä!")
     print("  ")
+    print(f"   Lensit koko pelin aikana yhteensä {kuljettu_matka:.0f} km")
+    print("")
     print("     PELAA UUDELLEEN - ENTER            SULJE PELI - 0")
     valinta = input("")
     while valinta != "" and valinta != "0":
@@ -261,69 +335,64 @@ def end():
         valinta = input("")
     return valinta
 
-# KOORDINAATTIEN HAKU
-
-# def hae_koordinaatit(airport_name):
-#    sql = "select latitude_deg, longitude_deg from airport where name = '" + airport_name + "'"
-#    cursor = yhteys.cursor()
-#    cursor.execute(sql)
-#    result = cursor.fetchall()
-#    return result
-
-# MATKAN PITUUDEN LASKEMINEN
-
-# def laske_välimatka(koordinaatit_1, koordinaatit_2):
-#    result = geopy.distance.geodesic(koordinaatit_1, koordinaatit_2).km
-#    return result
-
-# LENTOMATKAN PITUUS (omaksi funktioksi?)
-
-# lentokenttä_1 = lentokenttä, jossa oltiin viimeksi
-# lentokenttä_2 = uusi lentokenttä, joka valittiin seuraavaksi matkakohteeksi
-# koordinaatit_1 = hae_koordinaatit(lentokenttä_1)
-# koordinaatit_2 = hae_koordinaatit(lentokenttä_2)
-# välimatka = laske_välimatka(koordinaatit_1, koordinaatit_2)
-# kuljettu_matka = + välimatka
-# print(f"Kulkemasi matkan pituus: {kuljettu_matka:.0f} km")
-
 
 # ______________________ PÄÄOHJELMA ______________________
 
-# YHTEYS MYSQL:
-
 syöte = 1
-kuljettu_matka = 0
 
 yhteys = mysql.connector.connect(
          host='localhost',
          port= 3306,
          database='flight_game',
          user='root',
-         password='assiponi',
+         password='m!näk00d44n',
          autocommit=True
          )
 
 while syöte != "0":
+
     lennot = 1
     pisteet = 0
+    kuljettu_matka = 0
+
     valinta = mainmenu()  # ALOITUSRUUTU
     käydytmaat = [] # LISTAA KAIKKI MAAT JOSSA PELAAJA ON KÄYNYT
+    kaydyt_lentokentat = []
+
     if valinta == "0":
         break
+
     käyttäjänimi = käyttäjänimivalinta()    # PALAUTTAA KÄYTTÄJÄNIMEN
+
     arvotutmaat = arvokolmemaata()          # PALAUTTAA 3 MAATA
-    kotimaa = kotimaanvalinta(käyttäjänimi)             # PALAUTTAA KOTIMAAN
+
+    kotimaa = kotimaanvalinta(käyttäjänimi) # PALAUTTAA KOTIMAAN
+    kotimaa_lentokentta = hae_kotikentta(kotimaa[0])
     käydytmaat.append(kotimaa)
+    kaydyt_lentokentat.append(kotimaa_lentokentta)
     nykyinenmaa = kotimaa
+
     while lennot < 11:
+
         nykyinenmaa = arvolentokenttä(nykyinenmaa)  # PALAUTTAA NYKYISEN MAAN
         käydytmaat.append(nykyinenmaa[0])
-        id = hae_id(nykyinenmaa[0])                # HAKEE MAAN ID
-        kysymys_id = kysymys_pelaajalle(id, lennot)        # HAKEE KYSYMYKSEN PELAAJALLE
-        vastaukset = vastaus_vaihtoehdot(kysymys_id)  # HAKEE VASTAUSVAIHTOEHDOT PELAAJALLE
-        pelaajan_vastaus = anna_vastaus(vastaukset)  # PELAAJA SYÖTTÄÄ VASTAUKSEN -> OIKEIN/VÄÄRIN
+        kaydyt_lentokentat.append(nykyinenmaa[1])
+
+        id = hae_id(nykyinenmaa[0])
+        kysymys_id = kysymys_pelaajalle(id, lennot)
+        vastaukset = vastaus_vaihtoehdot(kysymys_id)
+        pelaajan_vastaus = anna_vastaus(vastaukset)
+
         pisteet = pisteidenlasku(pelaajan_vastaus, pisteet)
+
+        edellinen, nykyinen = edellinen_nykyinen_lentokentta(kaydyt_lentokentat)
+        koordinaatit_1 = hae_koordinaatit(edellinen)
+        koordinaatit_2 = hae_koordinaatit(nykyinen)
+        valimatka = laske_valimatka(koordinaatit_1, koordinaatit_2,)
+
         lennot = lennot + 1
+        kuljettu_matka += valimatka
+
     valinta = end()
 
     if valinta == "0":
